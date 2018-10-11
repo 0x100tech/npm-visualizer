829a243f079c10c90388c2bd551a9e83e39d4be7
Aleksandr Shevelev <shevelev.a@ati.su>
2018-10-11 14:21:21 +0300

    added linter step

--- src/dummy.test.js
+++ src/dummy.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const index = require('./index');
 
+/* eslint-disable no-undef */
 describe('dummy', () => {
   it('should return true', () => {
     assert.equal(true, index.dummy());
